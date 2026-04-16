d88150a7b8e3f890cd3b01351322c728bac5c76d
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-04-16 16:51:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Главный тумблер
-webview_power_state = "on"       # "on" / "off" — перед ревью Apple ставь "off"
+webview_power_state = "off"       # "on" / "off" — перед ревью Apple ставь "off"
 
 # Тумблер для фильтра
 use_hideclick = False      # "True" / "False" — включить или выключить фильтр
